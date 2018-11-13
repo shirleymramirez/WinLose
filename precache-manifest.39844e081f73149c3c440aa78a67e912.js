4c5d6dbb3a6d6d854ca3998a1860d4b0c52a50dc
unknown <shirley.mramirez@yahoo.com>
2018-11-13 10:36:20 -0700

    Updates

---
+++ precache-manifest.39844e081f73149c3c440aa78a67e912.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f2a70aec74431bfdb21",
+    "url": "/WinLose/static/js/runtime~main.4f2a70ae.js"
+  },
+  {
+    "revision": "2b299ca3f9c0afb1f562",
+    "url": "/WinLose/static/js/main.2b299ca3.chunk.js"
+  },
+  {
+    "revision": "c40ebc2deb63521971cc",
+    "url": "/WinLose/static/js/1.c40ebc2d.chunk.js"
+  },
+  {
+    "revision": "2b299ca3f9c0afb1f562",
+    "url": "/WinLose/static/css/main.7ac6d9c9.chunk.css"
+  },
+  {
+    "revision": "8e785ca0cf133c21217989678a361bd2",
+    "url": "/WinLose/index.html"
+  }
+];
